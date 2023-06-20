1bdb5ab19b2c2c4e64342736ab37ad8bd4578771
Raajheer1 <raaj.patel229@gmail.com>
2023-06-20 02:04:30 -0500

    Added admin & JSON download

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,38 @@
         <img alt="Verra Mobility" src="/src/assets/half_logo.jpg" class="w-32" />
       </div>
     </div>
-    <div v-if="!verified" class="w-full md:w-4/5 xl:w-1/2 mx-auto my-20">
+    <div v-if="admin" class="w-full md:w-4/5 xl:w-1/2 mx-auto my-20">
+      <div v-if="!admin_verified" class="bg-white rounded-2xl p-12">
+        <h1 class="text-xl font-semibold pb-10">Administrator Password:</h1>
+        <input class="w-1/2 border rounded p-2" type="password" v-model="password" placeholder="*****">
+        <br>
+        <br>
+        <button class="mt-2 rounded border border-[#a9c23f] py-2 px-4 hover:bg-[#a9c23f] hover:text-white transition" @click="submitPassword">
+          <span v-if="loading">Loading...</span>
+          <span v-else>Login</span>
+        </button>
+      </div>
+      <div v-else class="bg-white rounded-2xl p-12">
+        <h1 class="text-xl font-semibold pb-10">Users: <button class="ml-4 text-sm p-2 rounded-xl bg-gray-400" @click="downloadJSON">Download as JSON</button></h1>
+        <table class="table-auto w-full">
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Time Viewed</th>
+              <th>Date Added</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(user, idx) in users" :key="idx">
+              <td>{{ user.email }}</td>
+              <td>{{ user.time_viewed }}</td>
+              <td>{{ new Date(user.created_at).toLocaleDateString() }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div v-else-if="!verified" class="w-full md:w-4/5 xl:w-1/2 mx-auto my-20">
       <div class="bg-white rounded-2xl p-12">
         <h1 class="text-xl font-semibold pb-10">Please enter your email to start watching!</h1>
         <input class="w-1/2 border rounded p-2" type="email" v-model="email" placeholder="jane.doe@gmail.com">
@@ -26,7 +57,7 @@
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import axios from "axios";
 
 const email = ref<string>("");
@@ -34,6 +65,44 @@ const verified = ref<boolean>(false);
 const loading = ref<boolean>(false);
 const error = ref<string>("");
 
+const admin = ref<boolean>(false);
+const admin_verified = ref<boolean>(false);
+const password = ref<string>("");
+const users = ref<User[]>();
+
+type User = {
+  email: string;
+  time_viewed: number;
+}
+
+onMounted(() => {
+  //Check if route is /csv
+  if (window.location.pathname == "/admin") {
+    admin.value = true;
+  }
+})
+
+const submitPassword = async (): Promise<void> => {
+  if (password.value === "verra") {
+    admin_verified.value = true;
+    // const { data } = await axios.get("https://verra.live/api/emails");
+    const { data } = await axios.get("http://localhost:3000/api/emails");
+    users.value = data["emails"];
+  } else {
+    alert("Incorrect password.");
+  }
+}
+
+//Downloads Users as JSON
+const downloadJSON = (): void => {
+  const dataStr = JSON.stringify(users.value);
+  const dataUri = "data:application/json;charset=utf-8," + encodeURIComponent(dataStr);
+  const exportFileDefaultName = 'users.json';
+  const linkElement = document.createElement('a');
+  linkElement.setAttribute('href', dataUri);
+  linkElement.setAttribute('download', exportFileDefaultName);
+  linkElement.click();
+}
 
 const validEmail = ():boolean => {
   return email.value.includes("@");
